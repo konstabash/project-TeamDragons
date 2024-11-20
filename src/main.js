8b806f55d4db1d58a4fce9619cd6b760fbeb7d2a
Konstantin B <konstantin@bashuk.me>
2024-11-20 11:13:50 +0200

    footer review completed

--- src/main.js
+++
@@ -1,7 +0,0 @@
-<script>
-    fetch('./partials/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-container').innerHTML = data;
-    });
-</script>
